f4b8d93d6dc02adc8edda689a8e55831640289ed
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2025-07-30 20:22:22 +0600

    Signals modified for manage staff permissions

--- apps/authentication/signals.py
+++ apps/authentication/signals.py
@@ -7,23 +7,27 @@ from apps.authentication.utils.staffs_permissions import setup_staff_group_permi
 
 
 @receiver(post_save, sender=User)
-def enable_permissions_for_staffs(sender, instance, created, *args, **kwargs):
-    """Enabling permissions for staff users."""
+def manage_staff_group(sender, instance, created, **kwargs):
+    """Add or remove staff group based on is_staff flag, ignoring superusers."""
 
-    # If the user is not a staff or superuser, do nothing
-    # This is to ensure that only staff users get the permissions
-    if not instance.is_staff or instance.is_superuser:
+    # Ignore superusers (do not change groups here)
+    if instance.is_superuser:
         return
 
-    # Get or create the staff group
-    # If the group does not exist, it will be created with the necessary permissions
+    # Get or create the staff group with permissions
     staff_group = Group.objects.filter(name="staff").first()
     if not staff_group:
         staff_group = setup_staff_group_permissions()
 
-    def add_group_after_commit():
-        if staff_group not in instance.groups.all():
-            instance.groups.add(staff_group)
+    def update_groups():
+        if instance.is_staff:
+            # Add to staff group if not already in
+            if staff_group not in instance.groups.all():
+                instance.groups.add(staff_group)
+        else:
+            # Remove from staff group if present and no longer staff
+            if staff_group in instance.groups.all():
+                instance.groups.remove(staff_group)
 
     # Use transaction.on_commit to ensure the group is added after the transaction is committed
-    transaction.on_commit(add_group_after_commit)
+    transaction.on_commit(update_groups)
