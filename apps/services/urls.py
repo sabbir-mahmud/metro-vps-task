eca08164e837911e2e530b111dac1a14e423214d
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2025-08-01 08:09:59 +0600

    frontend basic ui design

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from apps.services import views
+
 urlpatterns = [
     path("api/v1/", include("apps.services.api.v1.urls")),
+    path("subscriptions/", views.subscriptions, name="subscriptions"),
 ]
--- apps/services/views.py
+++ apps/services/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+User = get_user_model()
+
+
+def subscriptions(request):
+    username_filter = request.GET.get("username", "").strip()
+
+    users_qs = User.objects.all()
+    if username_filter:
+        users_qs = users_qs.filter(username__icontains=username_filter)
+    users_qs = users_qs.prefetch_related("subscriptions__plan")
+
+    paginator = Paginator(users_qs, 5)
+    page_number = request.GET.get("page")
+    users_page = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "services/subscriptions.html",
+        {
+            "users": users_page,
+        },
+    )
