d4cfbc1d102b923d26ec5d48a68bcaaf929d1fd6
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2025-08-01 09:00:49 +0600

    Root url moved to subscriptions page

--- metrovps/urls.py
+++ metrovps/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("apps.authentication.urls")),
     path("services/", include("apps.services.urls")),
+    path("", RedirectView.as_view(url="/services/subscriptions/", permanent=False)),
 ]
